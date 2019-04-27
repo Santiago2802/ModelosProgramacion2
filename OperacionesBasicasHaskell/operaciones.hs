7d9bb8d4069d487ddd651400afec01e486066e6d
Gabelonio <gabelonio@hotmail.com>
2019-04-26 20:03:38 -0500

    Operaciones basicas con Haskell añadido

---
+++ OperacionesBasicasHaskell/operaciones.hs
@@ -0,0 +1,14 @@
+suma :: Integer -> Integer -> Integer
+suma a b = a+b
+
+cuadrado :: Integer -> Integer
+cuadrado a = a*a 
+
+resta :: Integer -> Integer -> Integer
+resta a b = a-b
+
+incremento :: Integer -> Integer
+incremento a = a+1
+
+decremento :: Integer -> Integer
+decremento a = a-1
